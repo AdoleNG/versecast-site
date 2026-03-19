fde6c8a0ce5ec07326fd135434233497ee316b4e
AdoleNG <victorameh2011@gmail.com>
2026-03-19 01:39:30 -0300

    Fix SupabaseClient import in Signup.jsx

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from "react";
-import { supabase } from "./supabaseClient";
+import { supabase } from "./SupabaseClient";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
