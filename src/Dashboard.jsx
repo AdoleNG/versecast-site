c784afa7c7c78900e3290df9b685829b2ea16824
AdoleNG <victorameh2011@gmail.com>
2026-03-21 16:31:13 -0300

    AcceptInvite fix7

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./supabaseClient.js";
+import { supabase } from "./supabaseClient.jsx";
 import VerseCastLogo from "./assets/VerseCastLogo.png";
 
 export default function Dashboard() {
